693cd5bf3ec96e4f5b766e5b44f523a4e5cfc810
Josh Justice <me@codingitwrong.com>
2024-03-18 07:18:03 -0400

    Set up rich text editor

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -4,4 +4,11 @@ class SupportRequestsController < ApplicationController
     @support_requests = SupportRequest.all
     puts "2222222"
   end
+
+  def update
+    support_request = SupportRequest.find(params[:id])
+    support_request.update(response: params.require(:support_request)[:response])
+    SupportRequestMailer.respond(support_request).deliver_now
+    redirect_to support_requests_path
+  end
 end
